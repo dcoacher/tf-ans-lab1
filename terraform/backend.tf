f7eefc36e728aab424c1ee88be32bea3aa0cf2e2
Desmond Coacher <dcoacher@outlook.com>
2025-09-18 20:55:04 +0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 # terraform {
 #   backend "s3" {
-#     bucket         = "tf-ans-lab1-s3-bucket"
+#     bucket         = "tf-ans-lab1-s3-bucket-2"
 #     key            = "global/s3/terraform.tfstate"
 #     region         = "us-east-1"
 #     dynamodb_table = "tf-ans-lab1-locks"
