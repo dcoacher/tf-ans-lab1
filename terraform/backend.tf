3342434d58e194ed06de0f486548b82c7b0bbbf6
Desmond Coacher <dcoacher@outlook.com>
2025-09-18 20:19:15 +0300

    Create backend.tf

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "tf-ans-lab1-s3-bucket"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf-ans-lab1-locks"
+    encrypt        = true
+  }
+}
